b57ed8ee9947260686b78eb00895a26d8fc8157b
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-01-07 16:05:11 +0100

    Add: Backend structure

---
+++ back-end/reserva-sales/src/main/java/ifc33b/dwesc/reserva_sales/ReservaSalesApplication.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.reserva_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaSalesApplication.class, args);
+	}
+
+}
---
+++ back-end/reserva-sales/src/test/java/ifc33b/dwesc/reserva_sales/ReservaSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.reserva_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
