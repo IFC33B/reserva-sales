f2b2ab0860fb678598ee6024adb78836c3c79946
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-01-07 16:37:34 +0100

    Create: ReservaRequest model

---
+++ back-end/reserva-sales/src/test/java/ifc33b/dwesc/reserva_sales/dto/ReservaRequest.java
@@ -0,0 +1,36 @@
+package ifc33b.dwesc.reserva_sales.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ReservaRequest {
+    // Atributos
+    @NotBlank(message = "El nombre de la sala es obligatorio")
+    private String nomSala;
+
+    @NotNull(message = "El dia es obligatorio")
+    private LocalDateTime dia;
+
+    @NotNull(message = "La hora es obligatoria")
+    @Min(value = 0, message = "La hora mínima es 0")
+    @Max(value = 23, message = "La hora máxima es 23")
+    private Integer hora;
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    private String usuari;
+
+    // Constructores
+    public ReservaRequest (String nomSala, LocalDateTime dia, Integer hora, String usuari) {
+        this.setNomSala(nomSala);
+        this.setDia(dia);
+        this.setHora(hora);
+        this.setUsuari(usuari);
+    }
+}
