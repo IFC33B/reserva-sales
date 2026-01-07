87adb775209d7dbcc5450b1611d11cb129a148cf
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-01-07 16:58:15 +0100

    Add(ReservaRepository.java): add query

--- back-end/reserva-sales/src/test/java/ifc33b/dwesc/reserva_sales/repository/ReservaRepository.java
+++ back-end/reserva-sales/src/test/java/ifc33b/dwesc/reserva_sales/repository/ReservaRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ifc33b.dwesc.reserva_sales.model.Reserva;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {}
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    Optional<Reserva> findByNomSalaAndDiaAndHora(String nomSala, LocalDateTime dia, Integer hora);
+}
