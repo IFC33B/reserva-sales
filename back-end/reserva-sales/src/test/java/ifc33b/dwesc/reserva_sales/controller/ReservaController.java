10e52379894eb20eb1558a7875c68e5be3b15015
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-01-07 17:10:51 +0100

    Update(ReservaController.java): complete ReservaController

--- back-end/reserva-sales/src/test/java/ifc33b/dwesc/reserva_sales/controller/ReservaController.java
+++ back-end/reserva-sales/src/test/java/ifc33b/dwesc/reserva_sales/controller/ReservaController.java
@@ -1,25 +1,45 @@
 package ifc33b.dwesc.reserva_sales.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ifc33b.dwesc.reserva_sales.dto.ReservaRequest;
+import ifc33b.dwesc.reserva_sales.dto.ReservaResponse;
+import ifc33b.dwesc.reserva_sales.service.ReservaService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/reserves")
 class ReservaController {
+    @Autowired
+    ReservaService reservaService;
+
     @GetMapping() // Devuelve las reservas
-    public int getReserves() {
-        return 0;
+    public ResponseEntity<List<ReservaResponse>> getReserves() {
+        // Service
+        List<ReservaResponse> response = reservaService.getReserves();
+
+        // HTTP Response
+        return ResponseEntity.ok(response);
     }
     
     @PostMapping() // Crea una reserva
-    public int createReserva(@RequestBody String entity) {
-        return 0;
+    public ResponseEntity<ReservaResponse> createReserva(@RequestBody ReservaRequest request) {
+        // Service
+        ReservaResponse response = reservaService.createReserva(request);
+
+        // HTTP Response
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
