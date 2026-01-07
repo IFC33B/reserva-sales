a4ce0c7e925f3c4b37234f51b7d0de83f1a6b101
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-01-07 17:02:53 +0100

    Create: ReservaService.java

---
+++ back-end/reserva-sales/src/test/java/ifc33b/dwesc/reserva_sales/service/ReservaService.java
@@ -0,0 +1,36 @@
+package ifc33b.dwesc.reserva_sales.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ifc33b.dwesc.reserva_sales.dto.ReservaRequest;
+import ifc33b.dwesc.reserva_sales.dto.ReservaResponse;
+import ifc33b.dwesc.reserva_sales.exception.SalaOcupadaException;
+import ifc33b.dwesc.reserva_sales.model.Reserva;
+import ifc33b.dwesc.reserva_sales.repository.ReservaRepository;
+
+@Service
+public class ReservaService {
+    @Autowired
+    ReservaRepository reservaRepository;
+
+    // Devuelve todas las reservas
+    public List<ReservaResponse> getReserves() {
+        return reservaRepository.findAll().stream()
+                .map(ReservaResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    // Crea una reserva
+    public ReservaResponse createReserva(ReservaRequest request) {
+        reservaRepository.findByNomSalaAndDiaAndHora(request.getNomSala(), request.getDia(), request.getHora())
+                .orElseThrow(() -> new SalaOcupadaException(request.getNomSala()));
+
+        Reserva reserva = new Reserva(request.getNomSala(), request.getDia(), request.getHora(), request.getUsuari());
+        reservaRepository.save(reserva);
+        return new ReservaResponse(reserva);
+    }
+}
